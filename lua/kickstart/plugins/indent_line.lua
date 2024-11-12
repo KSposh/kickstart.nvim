abd37d179abb6c438d530f2ea903b0718fa3d5d1
KSposh <49348402+KSposh@users.noreply.github.com>
2024-11-12 11:26:06 +0000

    Set custom indent configs

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -5,5 +5,19 @@ return {
     -- See `:help ibl`
     main = 'ibl',
     opts = {},
+    config = function()
+      local highlight = {
+        'CursorColumn',
+        'Whitespace',
+      }
+      require('ibl').setup {
+        indent = { highlight = highlight, char = '' },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        -- scope = { enabled = false },
+      }
+    end,
   },
 }
